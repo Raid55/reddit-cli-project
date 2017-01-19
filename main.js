5cd622d8af9b7d72ba765213caeaca6201c24ac0
Raid55 <boutboulnicholas@gmail.com>
2017-01-19 14:24:31 -0500

    big push, finally some stuff working

--- main.js
+++ main.js
@@ -24,26 +24,184 @@ function makeTopicList(subred){
   .then(function(res){
     nextP = res.data.after
     articleList = res.data.children.reduce(function(accu,el,indx){
-      accu.push({name: el.data.title ,value: 'MEMEBALLS'})
+      accu.push({name: el.data.title ,value: el.data.permalink})
       return accu;
     },[])
     return res
   })
-  // .then(function(res){
-  //   nextP = [{name: nextP,value: 'itaWORKS'}]
-  // })
+  .then(function(res){
+    articleList.push(new inquirer.Separator())
+    articleList.push({name: 'Next Page' ,value: "lawl"})
+    articleList.push(new inquirer.Separator())
+    return res
+  })
   .then(function(res){
     inquirer.prompt({
       type: 'list',
-      name: 'redditList',
-      message: 'What do you want to do?',
+      name: 'articles',
+      message: 'Which artile do you wish to read?',
       choices: articleList
-    }).then(
-      function(res) {
-        res.articleList
+    })
+    .then(function(res) {
+        // console.log('im here', res.articles)
+        return fetchRed.getArticle(res.articles)
+    })
+    .then(function(res){
+      return res.reduce(function(accu,el,indx){
+        if(indx === 0){
+          accu.push(el.data.children[0].data)
+        }
+        return accu
+      },[])
+    })
+    .then(function(res){
+      if(res[0].post_hint){
+        console.log(`=============================================================================`)
+        console.log('it was an image, images arent suported yet... or a link which is: ')
+        console.log(res[0].url)
+        console.log(`=============================================================================`)
+      }else{
+        console.log(res[0].selftext)
       }
-    );
-  });
+    })
+    .then(function(res){
+      inquirer.prompt({
+        type: 'list',
+        name: 'next',
+        message: 'What now?',
+        choices: whatNext
+      })
+      .then(function(res){
+        if(res.next == startOver){
+          res.next()
+        }else{
+          res.next(articleList,nextP)
+        }
+      })
+    })
+  })
+  .catch(function(err){
+    console.log('Error 55', err)
+    startOver()
+  })
+}
+
+function makeTopicListforHome(){
+  var articleList;
+  var nextP;
+  fetchRed.getHomepage()
+  .then(function(res){
+    nextP = res.data.after
+    articleList = res.data.children.reduce(function(accu,el,indx){
+      accu.push({name: el.data.title ,value: el.data.permalink})
+      return accu;
+    },[])
+    return res
+  })
+  .then(function(res){
+    articleList.push(new inquirer.Separator())
+    articleList.push({name: 'Next Page' ,value: "lawl"})
+    articleList.push(new inquirer.Separator())
+    return res
+  })
+  .then(function(res){
+    inquirer.prompt({
+      type: 'list',
+      name: 'articles',
+      message: 'Which artile do you wish to read?',
+      choices: articleList
+    })
+    .then(function(res) {
+        // console.log('im here', res.articles)
+        return fetchRed.getArticle(res.articles)
+    })
+    .then(function(res){
+      return res.reduce(function(accu,el,indx){
+        if(indx === 0){
+          accu.push(el.data.children[0].data)
+        }
+        return accu
+      },[])
+    })
+    .then(function(res){
+      if(res[0].post_hint){
+        console.log(`=============================================================================`)
+        console.log('it was an image, images arent suported yet... or a link which is: ')
+        console.log(res[0].url)
+        console.log(`=============================================================================`)
+      }else{
+        console.log(res[0].selftext)
+      }
+    })
+    .then(function(res){
+      inquirer.prompt({
+        type: 'list',
+        name: 'next',
+        message: 'What now?',
+        choices: whatNext
+      })
+      .then(function(res){
+        if(res.next == startOver){
+          res.next()
+        }else{
+          res.next(articleList,nextP)
+        }
+      })
+    })
+  })
+}
+
+function page(pageid){
+  fetchRed.nextPage(pageid)
+  .then
+}
+
+function goBack(articlArray,pageid){
+  var articleList = articlArray;
+  var nextP = pageid;
+  inquirer.prompt({
+    type: 'list',
+    name: 'articles',
+    message: 'Which artile do you wish to read?',
+    choices: articleList
+  })
+  .then(function(res) {
+      // console.log('im here', res.articles)
+      return fetchRed.getArticle(res.articles)
+  })
+  .then(function(res){
+    return res.reduce(function(accu,el,indx){
+      if(indx === 0){
+        accu.push(el.data.children[0].data)
+      }
+      return accu
+    },[])
+  })
+  .then(function(res){
+    if(res[0].post_hint){
+      console.log(`=============================================================================`)
+      console.log('it was an image, images arent suported yet... or a link which is: ')
+      console.log(res[0].url)
+      console.log(`=============================================================================`)
+    }else{
+      console.log(res[0].selftext)
+    }
+  })
+  .then(function(res){
+    inquirer.prompt({
+      type: 'list',
+      name: 'next',
+      message: 'What now?',
+      choices: whatNext
+    })
+    .then(function(res){
+      if(res.next == startOver){
+        res.next()
+      }else{
+        res.next(articleList,nextP)
+      }
+    })
+  })
 }
 
 
@@ -52,45 +210,74 @@ function makeSubredditInq(){
   fetchRed.getSubreddits()
   .then(function(res){
     redditList =res.data.children.reduce(function(accu,el,indx){
-      accu.push({name: el.data.url ,value: makeTopicList})
+      accu.push({name: el.data.url ,value: el.data.url})
       return accu;
     },[])
     return res
   })
   .then(function(res){
     inquirer.prompt({
-      type: 'rawlist',
-      name: 'redditList',
-      message: 'What do you want to do?',
+      type: 'list',
+      name: 'returnList',
+      message: 'Which subreddit do you want to open?',
       choices: redditList
     }).then(
       function(res) {
-        console.log(res.redditList);
+        makeTopicList(res.returnList);
       }
     );
   })
 }
+
+function askForCustomSub(){
+  inquirer.prompt({
+    type: 'input',
+    name: 'response',
+    message: 'Which Subreddit would you like to browse?(Cap sensitive/in this format: /r/whatever_sub/)',
+  })
+  .then(
+    function(res) {
+      console.log(res.response)
+      makeTopicList(res.response)
+    }
+  );
+}
+
+
 ////////////////////////////////////////////////////////////////
 
 //
 // fetchRed.getHomepage().then(function(res){
 //   console.log(res.data.children[0])
 // })
-var redditList = makeSubredditInq()
+// var redditList = makeSubredditInq()
+
+
+var whatNext = [
+  {name: 'Go back', value: goBack},
+  new inquirer.Separator(),
+  {name: 'Start Over', value: startOver}
+]
+
 
 var start = [
-  {name: 'Show homepage', value: 'HOMEPAGE'},
-  {name: 'Custom subreddit', value: 'lolol'},
+  {name: 'Show homepage', value: makeTopicListforHome},
+  {name: 'Custom subreddit', value: askForCustomSub},
   {name: 'List subreddits', value: makeSubredditInq}
 ];
 
-inquirer.prompt({
-  type: 'list',
-  name: 'start',
-  message: 'What do you want to do?',
-  choices: start
-}).then(
-  function(res) {
-    res.start()
-  }
-);
+function startOver(){
+  inquirer.prompt({
+    type: 'list',
+    name: 'start',
+    message: 'What do you want to do?',
+    choices: start
+  })
+  .then(
+    function(res) {
+      res.start()
+    }
+  );
+}
+
+startOver()
